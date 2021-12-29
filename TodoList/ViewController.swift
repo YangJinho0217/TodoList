e8501993fc04116f75e0840baaba9557ab335870
YagSill <73631818+YangJinho0217@users.noreply.github.com>
2021-12-29 17:13:55 +0900

    커밋

--- TodoList/ViewController.swift
+++ TodoList/ViewController.swift
@@ -6,14 +6,51 @@
 //
 
 import UIKit
+import Lottie
 
 class ViewController: UIViewController {
+    
+    var titleLable : UILabel = {
+        
+        let label = UILabel()
+        label.text = "메인화면"
+        label.textColor = .black
+        label.font = UIFont.boldSystemFont(ofSize: 70)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    let animationView : AnimationView = {
+        
+        let animview = AnimationView(name: "88770-loading-boxes")
+        animview.frame = CGRect(x: 0, y: 0, width: 400, height: 400)
+        animview.contentMode = .scaleAspectFill
+        return animview
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(animationView)
+        animationView.center = view.center
+        
+        animationView.play{ ( finish) in
+            
+            print("애니메이션 끝")
+            
+            self.animationView.removeFromSuperview()
+            
+            
+            
+            self.view.addSubview(self.titleLable)
+            self.titleLable.translatesAutoresizingMaskIntoConstraints = false
+            self.titleLable.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+            self.titleLable.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        }
     }
 
-
+    
+   
 }
 
